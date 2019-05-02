30eb71bde2a3a057425bcbeb0b47666a31bb91bd
katerina <katerina_sanway@mail.ru>
2019-05-02 21:21:58 +0300

    1. Добавлены значения по умолчанию для аргументов функции 2. Убраны запятые после каждого товара

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const goods = [
   { title: "Socks", price: 50 },
   { title: "Jacket", price: 350 },
   { title: "Shoes", price: 250 }
-]
+];
 
-const renderGoodsItem = (title, price) => {
+const renderGoodsItem = (title = "", price = 0) => {
   return `<div class="goods-item">
     <h3>${title}</h3>
     <p>${price}</p>
   </div>`;
-}
+};
 
 const renderGoodsList = (list) => {
   const goodsList = list.map(item =>
     renderGoodsItem(item.title, item.price));
-  document.querySelector('.goods-list').innerHTML = goodsList;
-}
+  document.querySelector('.goods-list').innerHTML = goodsList.join('');
+};
 
-renderGoodsList(goods)
+renderGoodsList(goods);
