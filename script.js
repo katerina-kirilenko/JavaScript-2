f0a57e748f3aa461be51740a64dd3efccccd3de0
katerina <katerina_sanway@mail.ru>
2019-05-10 20:13:12 +0300

    Реализация списка товаров через классы

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
-const goods = [
-  { title: "Shirt", price: 150 },
-  { title: "Socks", price: 50 },
-  { title: "Jacket", price: 350 },
-  { title: "Shoes", price: 250 }
-];
-
-const renderGoodsItem = (title = "", price = 0) => {
-  return `<div class="goods-item">
+class GoodsItem {
+  constructor(title = "Без названия", price = 0) {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `<div class="goods-item">
     <img src="photo.png" alt="product" class="img-product">
-    <h3>${title}</h3>
-    <p>${price} руб.</p>
+    <h3>${this.title}</h3>
+    <p>${this.price} руб.</p>
     <button class="button-item">Добавить</button>
-  </div>`;
-};
+  </div>` ;
+  }
+}
 
-const renderGoodsList = (list) => {
-  const goodsList = list.map(item =>
-    renderGoodsItem(item.title, item.price));
-  document.querySelector('.goods-list').innerHTML = goodsList.join('');
-};
+class GoodsList {
+  constructor() {
+    this.goods = [];  
+  }
+  fetchGoods() {
+    this.goods = [
+      { title: "Shirt", price: 150 },
+      { title: "Socks", price: 50 },
+      { title: "Jacket", price: 350 },
+      { price: 250 },
+      { title: "Shirt", price: 150 },
+      { title: "Socks", price: 50 },
+      { title: "Jacket", price: 350 },
+      { title: "Shoes" }
+    ];
+  }
+  render() {
+    let listHtml = '';
+    this.goods.forEach( good => {
+      const goodItem = new GoodsItem(good.title, good.price);
+      listHtml += goodItem.render();
+    });
+    document.querySelector('.goods-list').innerHTML = listHtml;
+  }
+}
 
-renderGoodsList(goods);
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
